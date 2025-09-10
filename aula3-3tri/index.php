20103fa6ac14ec4ea29bebab5f1a632e158a5d8c
alexandre <alexandre.inascimento@aluno.edu.es.gov.br>
2025-09-10 11:07:57 -0300

    Create index.php

---
+++ aula3-3tri/index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title>Aula 03 php</title>
+    </head>
+    <body>
+        <?php
+      $nome1="gustavo";
+      $nome2="alexandre";
+
+      $n1=10;
+      $n2=2;
+      $soma= $n1+$n2;
+
+      echo "Meu nome é " .$nome1 ." é  minha dupla é " .$nome2;
+      echo "<br> Meu nome é $nome1 e minha dupla é $nome2";
+
+      echo "<br> <br> A soma de $n1 com $n2 é iqual a $soma.";
+
+      echo "<br> <br> o resultado da subtração de $n1 com $n2 é iqual a: ".($n1-$n2) .".";
+
+      echo "<br> <br> o resutado da mutiplicação de $n1 com $n2 é iqual a:".($n1*$n2).".";
+
+      echo "<br> <br> o resutado da divisão real de $n1 com $n2 é iqual a:".($n1/$n2).".";
+
+      echo "<br> <br> o resto da divisão real de $n1 com $n2 é iqual a:".($n1%$n2).".";
+
+
+       ?>
+    </body>
+</html>
